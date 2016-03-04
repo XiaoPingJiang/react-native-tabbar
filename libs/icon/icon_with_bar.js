7374166fdc01d04a4071f762bdb3f56462b5f265
XiaoPingJiang <jyx2011719@hotmail.com>
2016-03-04 22:16:03 +0800

    修改tab支持图标

--- libs/icon/icon_with_bar.js
+++ libs/icon/icon_with_bar.js
@@ -1,111 +1,113 @@
 import React, {
-  StyleSheet,
-  Component,
-  View,
-  Text,
-  TouchableWithoutFeedback
+    StyleSheet,
+    Component,
+    View,
+    Text,
+    TouchableWithoutFeedback,
+    Image
 } from 'react-native';
 import { extendRawIcon } from './raw';
 
 const styles = StyleSheet.create({
-  icon: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
+    icon: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 })
 
 class IconWithBar extends Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      selected: false
-    };
-  }
-
-  onPress() {
-    const { tabName, gotoTab } = this.context;
-    gotoTab(tabName);
-  }
-
-  tabDidActive() {
-    this.setState({ selected: true });
-    //console.log(`tab ${this.context.tabName} is active`);
-  }
-
-  tabDidInactive() {
-    this.setState({ selected: false });
-    //console.log(`tab ${this.context.tabName} is inactive`);
-  }
-
-  render() {
-    const {
-      label,
-      type,
-      from,
-      size,
-      iconStyle,
-      onActiveColor,
-      onInactiveColor,
-      onActiveColorBar,
-      onInactiveColorBar
-    } = this.props;
-    const { selected } = this.state;
-
-    const color = selected? onActiveColor : onInactiveColor
-    const barColor = selected? onActiveColorBar : onInactiveColorBar;
-    const borderWidth = selected? 2 : 1;
-    const padding = selected? 0 : 1;
-
-    let icon = null;
-    if (!!type && !from) {
-      throw new Error("icon must contains 'type' and 'from' values");
-    } else if (!type && !!from) {
-      throw new Error("icon must contains 'type' and 'from' values");
-    } else if (!!type && !!from) {
-      icon = (
-        <Text style={[iconStyle, { fontSize: size, fontFamily: from, color: color }]}>
-          {type}
-        </Text>
-      );
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            selected: false
+        };
     }
 
-    return (
-      <TouchableWithoutFeedback style={{ flex: 1 }} onPress={this.onPress.bind(this)}>
-        <View style={[styles.icon, { borderTopWidth: borderWidth, borderTopColor: barColor, paddingTop: padding }]}>
-          {icon}
-          <View style={{ paddingTop: 5 }}>
-            <Text style={{ fontSize: 12, color: color }}>{label}</Text>
-          </View>
-        </View>
-      </TouchableWithoutFeedback>
-    );
-  }
+    onPress() {
+        const { tabName, gotoTab } = this.context;
+        gotoTab(tabName);
+    }
+
+    tabDidActive() {
+        this.setState({selected: true});
+        //console.log(`tab ${this.context.tabName} is active`);
+    }
+
+    tabDidInactive() {
+        this.setState({selected: false});
+        //console.log(`tab ${this.context.tabName} is inactive`);
+    }
+
+    render() {
+        const {
+            label,
+            iconSource,
+            iconSourceOnActive,
+            from,
+            size,
+            iconStyle,
+            onActiveColor,
+            onInactiveColor,
+            onActiveColorBar,
+            onInactiveColorBar
+            } = this.props;
+        const { selected } = this.state;
+
+        const color = selected ? onActiveColor : onInactiveColor
+        const barColor = selected ? onActiveColorBar : onInactiveColorBar;
+        const borderWidth = 1; // selected? 2 : 1;
+        const padding = selected ? 0 : 1;
+
+        let icon = null;
+        if (!!iconSource && !iconSourceOnActive) {
+            throw new Error("icon must contains 'iconSource' and 'iconSourceOnActive' values");
+        } else if (!iconSource && !!iconSourceOnActive) {
+            throw new Error("icon must contains 'iconSource' and 'iconSourceOnActive' values");
+        } else if (!!iconSource && !!iconSourceOnActive) {
+            var source = selected ? iconSource : iconSourceOnActive;
+            icon = (<Image source={source}></Image>);
+        }
+
+        return (
+            <TouchableWithoutFeedback style={{flex: 1}} onPress={this.onPress.bind(this)}>
+                <View style={[styles.icon,{borderTopWidth: borderWidth, borderTopColor:barColor, paddingTop: padding}]}>
+                    {icon}
+                    <View>
+                        <Text style={{fontSize: 12}}>
+                            {label}
+                        </Text>
+                    </View>
+                </View>
+            </TouchableWithoutFeedback>
+        );
+    }
 }
 
 IconWithBar.propTypes = {
-  label: React.PropTypes.string,
-  type: React.PropTypes.string,
-  from: React.PropTypes.string,
-  size: React.PropTypes.number,
-  iconStyle: React.PropTypes.any,
-  onActiveColor: React.PropTypes.string,
-  onInactiveColor: React.PropTypes.string,
-  onActiveColorBar: React.PropTypes.string,
-  onInactiveColorBar: React.PropTypes.string
+    label: React.PropTypes.string,
+    iconSource: React.PropTypes.number,
+    iconSourceOnActive: React.PropTypes.number,
+    from: React.PropTypes.string,
+    size: React.PropTypes.number,
+    iconStyle: React.PropTypes.any,
+    onActiveColor: React.PropTypes.string,
+    onInactiveColor: React.PropTypes.string,
+    onActiveColorBar: React.PropTypes.string,
+    onInactiveColorBar: React.PropTypes.string
 };
 
 IconWithBar.defaultProps = {
-  size: 20,
-  onActiveColor: 'white',
-  onInactiveColor: 'black',
-  onActiveColorBar: 'red',
-  onInactiveColorBar: 'gray'
+    size: 20,
+    onActiveColor: 'white',
+    onInactiveColor: 'black',
+    onActiveColorBar: 'gray',
+    onInactiveColorBar: 'gray'
 };
 
 IconWithBar.contextTypes = {
-  tabName: React.PropTypes.string,
-  gotoTab: React.PropTypes.func
+    tabName: React.PropTypes.string,
+    gotoTab: React.PropTypes.func
 };
 
 export default extendRawIcon(IconWithBar);
